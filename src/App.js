653da9354197fb4a6793fd342484b33145289ee2
oskar <atorpos@mac.com>
2023-02-20 16:48:37 +0800

    new text added

--- src/App.js
+++ src/App.js
@@ -4,10 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <nav className="navbar background">
+          <ul class="nav-list">
+              <div class="logo"><img src="logo192.png" /></div>
+              <li><a href="#home">Home</a></li>
+              <li><a href="#aboutus">About Us</a></li>
+          </ul>
+      </nav>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. New site is coming.
         </p>
         <a
           className="App-link"
